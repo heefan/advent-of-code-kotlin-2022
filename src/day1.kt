975c6d5b7fd5b9557f249a5157f3cba024f0e137
LI Tian <li_tian@singaporeair.com.sg>
2022-12-01 23:02:00 +0800

    chore: move non source code file to resources folder

--- src/day1.kt
+++ src/day1.kt
@@ -4,7 +4,7 @@ fun main() {
     var maxValue = 0
     var curValue = 0
 
-    File("./day1.txt").useLines { lines ->
+    File("resources/day1.txt").useLines { lines ->
         lines.forEach {
             if(it.isBlank()) {
                maxValue = maxOf(maxValue, curValue)
