56a6600ade450146268a59f8c4bfeb5acd62e1e6
LI Tian <li_tian@singaporeair.com.sg>
2022-12-04 21:12:09 +0800

    day1: #refactor, add most concise code

--- src/day1.kt
+++ src/day1.kt
@@ -1,30 +1,18 @@
-
 fun main() {
-    val day1 = Day1()
-    val input = readInput("input1")
+    val input = readInputAsText("input1")
 
-    check(day1.part1(input) { handler(input) } == 69310)
-    check(day1.part2(input) { handler(input) } == 206104)
+    check(part1(input) == 69310)
+    check(part2(input) == 206104)
 }
 
-class Day1 {
-    //todo:  How to avoid the duplicate declaration of the handling?
-    fun part1(arg0: List<String>, handler: (arg1: List<String>) -> List<Int>): Int {
-        return handler(arg0).max()
-    }
-    fun part2(arg0: List<String>, handler: (arg1: List<String>) -> List<Int>): Int {
-        return handler(arg0).sortedDescending().take(3).sum()
-    }
-}
+fun part1(input: String): Int = input
+    .split("\n\n")
+    .maxOf { it.split("\n").sumOf(String::toInt)}
 
-fun handler(input: List<String>) : List<Int> {
-    var sums = mutableListOf(0)
-    input.forEach {
-        if (it.isBlank()) {
-            sums.add(0)
-        } else {
-            sums[sums.lastIndex] += it.toInt()
-        }
-    }
-    return sums
-}
+
+fun part2(input: String): Int = input
+    .split("\n\n")
+    .map { it.split("\n").sumOf(String::toInt)}
+    .sortedDescending()
+    .take(3)
+    .sum()
